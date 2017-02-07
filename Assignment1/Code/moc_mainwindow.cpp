9317aabb0c5ca74dad36e58f5ff4ff4ee7ca7c9a
YerongLeopard <yerong.li@outlook.com>
2017-02-06 23:04:33 -0800

    Mac qt5 test

---
+++ Assignment1/Code/moc_mainwindow.cpp
@@ -0,0 +1,209 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.8.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[26];
+    char stringdata0[299];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 9), // "OpenImage"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 9), // "SaveImage"
+QT_MOC_LITERAL(4, 32, 16), // "SaveDisplayImage"
+QT_MOC_LITERAL(5, 49, 10), // "ResetImage"
+QT_MOC_LITERAL(6, 60, 11), // "ToggleImage"
+QT_MOC_LITERAL(7, 72, 8), // "AddNoise"
+QT_MOC_LITERAL(8, 81, 15), // "BlackWhiteImage"
+QT_MOC_LITERAL(9, 97, 13), // "MeanBlurImage"
+QT_MOC_LITERAL(10, 111, 17), // "GaussianBlurImage"
+QT_MOC_LITERAL(11, 129, 15), // "FirstDerivImage"
+QT_MOC_LITERAL(12, 145, 16), // "SecondDerivImage"
+QT_MOC_LITERAL(13, 162, 12), // "SharpenImage"
+QT_MOC_LITERAL(14, 175, 10), // "SobelImage"
+QT_MOC_LITERAL(15, 186, 9), // "HalfImage"
+QT_MOC_LITERAL(16, 196, 11), // "RotateImage"
+QT_MOC_LITERAL(17, 208, 10), // "Brightness"
+QT_MOC_LITERAL(18, 219, 3), // "val"
+QT_MOC_LITERAL(19, 223, 4), // "Zoom"
+QT_MOC_LITERAL(20, 228, 6), // "Scroll"
+QT_MOC_LITERAL(21, 235, 14), // "BilateralImage"
+QT_MOC_LITERAL(22, 250, 14), // "FindPeaksImage"
+QT_MOC_LITERAL(23, 265, 11), // "MedianImage"
+QT_MOC_LITERAL(24, 277, 10), // "HoughImage"
+QT_MOC_LITERAL(25, 288, 10) // "CrazyImage"
+
+    },
+    "MainWindow\0OpenImage\0\0SaveImage\0"
+    "SaveDisplayImage\0ResetImage\0ToggleImage\0"
+    "AddNoise\0BlackWhiteImage\0MeanBlurImage\0"
+    "GaussianBlurImage\0FirstDerivImage\0"
+    "SecondDerivImage\0SharpenImage\0SobelImage\0"
+    "HalfImage\0RotateImage\0Brightness\0val\0"
+    "Zoom\0Scroll\0BilateralImage\0FindPeaksImage\0"
+    "MedianImage\0HoughImage\0CrazyImage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      23,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  129,    2, 0x08 /* Private */,
+       3,    0,  130,    2, 0x08 /* Private */,
+       4,    0,  131,    2, 0x08 /* Private */,
+       5,    0,  132,    2, 0x08 /* Private */,
+       6,    0,  133,    2, 0x08 /* Private */,
+       7,    0,  134,    2, 0x08 /* Private */,
+       8,    0,  135,    2, 0x08 /* Private */,
+       9,    0,  136,    2, 0x08 /* Private */,
+      10,    0,  137,    2, 0x08 /* Private */,
+      11,    0,  138,    2, 0x08 /* Private */,
+      12,    0,  139,    2, 0x08 /* Private */,
+      13,    0,  140,    2, 0x08 /* Private */,
+      14,    0,  141,    2, 0x08 /* Private */,
+      15,    0,  142,    2, 0x08 /* Private */,
+      16,    0,  143,    2, 0x08 /* Private */,
+      17,    1,  144,    2, 0x08 /* Private */,
+      19,    1,  147,    2, 0x08 /* Private */,
+      20,    1,  150,    2, 0x08 /* Private */,
+      21,    0,  153,    2, 0x08 /* Private */,
+      22,    0,  154,    2, 0x08 /* Private */,
+      23,    0,  155,    2, 0x08 /* Private */,
+      24,    0,  156,    2, 0x08 /* Private */,
+      25,    0,  157,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->OpenImage(); break;
+        case 1: _t->SaveImage(); break;
+        case 2: _t->SaveDisplayImage(); break;
+        case 3: _t->ResetImage(); break;
+        case 4: _t->ToggleImage(); break;
+        case 5: _t->AddNoise(); break;
+        case 6: _t->BlackWhiteImage(); break;
+        case 7: _t->MeanBlurImage(); break;
+        case 8: _t->GaussianBlurImage(); break;
+        case 9: _t->FirstDerivImage(); break;
+        case 10: _t->SecondDerivImage(); break;
+        case 11: _t->SharpenImage(); break;
+        case 12: _t->SobelImage(); break;
+        case 13: _t->HalfImage(); break;
+        case 14: _t->RotateImage(); break;
+        case 15: _t->Brightness((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->Zoom((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->Scroll((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->BilateralImage(); break;
+        case 19: _t->FindPeaksImage(); break;
+        case 20: _t->MedianImage(); break;
+        case 21: _t->HoughImage(); break;
+        case 22: _t->CrazyImage(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 23)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 23;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 23)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 23;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
