2cf15627ec55ff0850351f8d8635dd5441a3f2b5
YerongLeopard <yerong.li@outlook.com>
2017-02-15 10:45:53 -0800

    convolution

--- Assignment1/Code/Project1.cpp
+++ Assignment1/Code/Project1.cpp
@@ -28,25 +28,26 @@ void convolve(QImage *image, double *kernel, int kernelHeight, int kernelWidth, 
     int width = image->width();
     int kernelHalfHeight = (kernelHeight-1)/2;  //DEBUG
     int kernelHalfWidth = (kernelWidth-1)/2;
+    int x,y,bx,by,fx,fy;
     // Create an empty image
     QImage buffer = image->copy(-kernelHalfWidth, -kernelHalfHeight, width+2*kernelHalfWidth, height+2*kernelHalfHeight);
     /*  QImage QImage::copy(const QRect & rectangle = QRect()) const
      *  The returned image is copied from the position (x, y) in this image, and will always have the given width and heig
 ht.
      *  In areas beyond this image, pixels are set to 0.*/
-    for (int y= 0; y<height; y++)
+    for (y= 0; y<height; y++)
     {
-        for (int x= 0; x<width; x++)
+        for (x= 0; x<width; x++)
         {
             double rgb[3];
             rgb[0]= rgb[1]= rgb[2]= (!isDerivative? 0.0 : 128.0);//derivative standard
 
-            for (int fy= 0; fy < kernelHeight; fy++)
+            for (fy= 0; fy < kernelHeight; fy++)
             {
-                for (int fx= 0; fx < kernelWidth; fx++)
+                for (fx= 0; fx < kernelWidth; fx++)
                 {
-                    int by= y + fy;
-                    int bx= x + fx;
+                    by= y + fy;
+                    bx= x + fx;
                     QRgb pixel= buffer.pixel(bx, by);
                     double weight= kernel[fy*kernelWidth+fx];
                     rgb[0]+= weight*qRed(pixel);
